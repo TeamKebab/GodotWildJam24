341a5ea57d7b7744d93720183fd0506888dbd6fa
Ángela López <angela.lbsr@gmail.com>
2020-08-16 13:53:00 +0200

    monster faces any direction

--- src/entities/monsters/Monster.gd
+++ src/entities/monsters/Monster.gd
@@ -1,3 +1,4 @@
+tool
 extends KinematicBody2D
 
 
@@ -5,10 +6,12 @@ const HP = preload("res://src/components/HP.gd")
 
 
 export var max_hp: int = 1 
-
+export var facing_direction: Vector2 = Vector2.DOWN setget _set_facing_direction
 
 var motion = Vector2.ZERO
 
+
+onready var animationTree : AnimationTree = $AnimationTree
 
 onready var hp = HP.new(max_hp)
 onready var start_position = position
@@ -16,9 +19,11 @@ onready var start_position = position
 
 func _ready():
 	hp.connect("died", self, "_die")	
-
+	
+	animationTree.set("parameters/blend_position", facing_direction)
 
 func _physics_process(delta):
+		
 	motion = move_and_slide(motion)
 	
 	
@@ -29,3 +34,10 @@ func restart():
 func _die():
 	queue_free()
 
+
+func _set_facing_direction(new_direction):
+	facing_direction = new_direction
+	
+	if $AnimationTree != null:
+		$AnimationTree.set("parameters/blend_position", facing_direction)
+	
